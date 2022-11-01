5d3b6e27154cf827a731050b84821e5e2c1f2118
Derlidio Siqueira <derlidio@users.noreply.github.com>
2022-10-31 21:05:52 -0300

    First Commit

---
+++ EZSQLite.cs
@@ -0,0 +1,44 @@
+﻿/*__ ____  _             
+| __|_  / /_\  _ __ _ __ 
+| _| / / / _ \| '_ \ '_ \
+|___/___/_/ \_\ .__/ .__/
+|  \/  |__ _| |_|__|_| _ 
+| |\/| / _` | / / -_) '_|
+|_|  |_\__,_|_\_\___|_|
+ 
+(C)2022 Derlidio Siqueira - Expoente Zero */
+
+using System;
+using System.IO;
+
+using Microsoft.Data.Sqlite;
+
+namespace EZForms
+{
+	public static class EZSQLite
+	{
+        static EZSQLite()
+		{
+			SQLitePCL.Batteries.Init();
+		}
+
+		public static SqliteConnection Connect(string db)
+        {
+			SqliteConnection connection = null;
+
+			if (!string.IsNullOrWhiteSpace(db))
+            {
+				string database = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), db.Trim());
+
+				try
+                {
+					connection = new SqliteConnection($"Data Source={database}");
+				}
+                catch { /* Dismiss */ }
+			}
+
+			return connection;
+		}
+	}
+}
+
---
+++ README.md
@@ -0,0 +1,9 @@
+﻿# EZForms
+
+*EZForms* is a basic set of helper classes that integrates with EZAppMaker. It's aimed
+to work with SQLite and will take care of all your needs on the CRUD regard. You won't have
+to write a single SQL statement. You won't have to create models for your tables. Yow
+wont't have to bind to Entity Framework. Just plug your SQLite database and write a markup
+file (XML) to tell EZForms what you want to see, how you want to see it, and that's it.
+
+EZForms is still under development, but it's current version can handle single-record forms pretty well.
